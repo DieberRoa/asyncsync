5d5c9f06c041f06735782cb32a7495e4697ff66d
Usuario aplicaciones Apps <apps@pragma.com.co>
2024-01-03 10:01:21 -0500

    Se agregan variables de SMTP

--- src/main/java/dieberroa/demo/asyncsync/api/ImpSendMail.java
+++ src/main/java/dieberroa/demo/asyncsync/api/ImpSendMail.java
@@ -1,41 +1,36 @@
 package dieberroa.demo.asyncsync.api;
 import javax.mail.*;
 import javax.mail.internet.*;
+
+import dieberroa.demo.asyncsync.common.EmailConstants;
+
 import java.util.Properties;
 
 public class ImpSendMail {
 
     public Boolean sendEmail(String recipient, String subject, String content) {
-        // Configuración de las propiedades del servidor SMTP
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.example.com"); // Cambiar al host de tu servidor SMTP
-        properties.put("mail.smtp.port", "587"); // Cambiar al puerto del servidor SMTP
-        properties.put("mail.smtp.ssl.trust", "smtp.example.com"); // Cambiar al host de tu servidor SMTP
+        properties.put("mail.smtp.host", EmailConstants.SMTP_HOST);
+        properties.put("mail.smtp.port", EmailConstants.SMTP_PORT);
 
-        // Credenciales del remitente
-        final String username = "tu_usuario@example.com"; // Cambiar por tu dirección de correo
-        final String password = "tu_contraseña"; // Cambiar por tu contraseña
-
-        // Crear una sesión con las propiedades y las credenciales
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+                return new PasswordAuthentication(EmailConstants.SENDER_EMAIL, EmailConstants.SENDER_PASSWORD);
             }
         });
 
         try {
-            // Crear un mensaje con el contenido y el destinatario
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
+            message.setFrom(new InternetAddress(EmailConstants.SENDER_EMAIL));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
             message.setSubject(subject);
             message.setText(content);
 
-            // Enviar el correo
             Transport.send(message);
+            System.out.println("Correo enviado con éxito a " + recipient);
             return true;
         } catch (MessagingException e) {
             e.printStackTrace();
